567b9e86f3adad44d1a1970e08fee01dc71f957b
Pradumna <pradumnabajoria@gmail.com>
2021-06-13 17:18:07 +0530

    added topbar component

---
+++ src/component/topbar/TopBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./TopBar.css"
+
+function TopBar() {
+    return (
+        <div className='top'>
+            TopBar
+        </div>
+    )
+}
+
+export default TopBar
