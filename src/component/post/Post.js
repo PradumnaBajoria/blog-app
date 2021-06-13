9988073c7cff3506b170853929de4492b2511b47
Pradumna <pradumnabajoria@gmail.com>
2021-06-14 00:38:36 +0530

    completed post components

---
+++ src/component/post/Post.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import "./Post.css"
+
+function Post() {
+    return (
+        <div className='post'>
+            <img
+                className='postImg'
+                alt='img'
+                src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg' 
+            />
+            <div className='postInfo'>
+                <div className='postCats'>
+                    <span className='postCat'>Anime</span>
+                    <span className='postCat'>Music</span>
+                </div>
+                <span className='postTitle'>
+                    Hello There
+                </span>
+                <hr />
+                <span className='postDate'>1 hour ago</span>
+            </div>
+            <p className='postDesc'>
+                How are you all doing, this is the first blog. hope you like it.
+                How are you all doing, this is the first blog. hope you like it.
+                How are you all doing, this is the first blog. hope you like it.
+                How are you all doing, this is the first blog. hope you like it.
+                How are you all doing, this is the first blog. hope you like it.
+                How are you all doing, this is the first blog. hope you like it.
+                How are you all doing, this is the first blog. hope you like it.
+                How are you all doing, this is the first blog. hope you like it.
+                How are you all doing, this is the first blog. hope you like it.
+                How are you all doing, this is the first blog. hope you like it.
+                How are you all doing, this is the first blog. hope you like it.
+                How are you all doing, this is the first blog. hope you like it.
+            </p>
+        </div>
+    )
+}
+
+export default Post
