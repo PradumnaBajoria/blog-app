c85e98a3f9e01280f02a6d59804de6ad1e68e54e
Pradumna <pradumnabajoria@gmail.com>
2021-06-14 17:43:21 +0530

    added manual routing

--- src/App.js
+++ src/App.js
@@ -5,13 +5,39 @@ import Register from "./pages/register/Register";
 import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <TopBar />
-      <Register />
+      <Router>
+        <TopBar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/settings">
+            <Settings />
+          </Route>
+          <Route path="/write">
+            <Write />
+          </Route>
+          <Route path="/post/:postId">
+            <Single />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
