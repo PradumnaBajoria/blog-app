11c4f2994c7ea24d945d8c30eda93d7a7c7de4bb
Pradumna <pradumnabajoria@gmail.com>
2021-06-15 17:41:05 +0530

    completed login and register routes for authentication

--- api/index.js
+++ api/index.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const dotenv = require("dotenv")
 const mongoose = require("mongoose")
+const authRoute = require('./routes/auth')
 
 const app = express()
 
 dotenv.config()
+
+app.use(express.json())
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true, 
@@ -13,6 +16,8 @@ mongoose.connect(process.env.MONGO_URL, {
 })
     .then(console.log('connected to MongoDB'))
     .catch(err => console.log(err));
+
+app.use("/api/auth", authRoute)
 
 
 app.listen("5000", () => {
