86e2966f7b017002a7a06dde82b0e70628c59add
Pradumna <pradumnabajoria@gmail.com>
2021-06-15 23:33:34 +0530

    completed categories router

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose")
 const authRoute = require('./routes/auth')
 const userRoute = require('./routes/users')
 const postRoute = require('./routes/posts')
+const categoryRoute = require('./routes/categories')
 
 const app = express()
 
@@ -22,6 +23,7 @@ mongoose.connect(process.env.MONGO_URL, {
 app.use("/api/auth", authRoute)
 app.use("/api/users", userRoute)
 app.use("/api/posts", postRoute)
+app.use("/api/categories", categoryRoute)
 
 
 app.listen("5000", () => {
