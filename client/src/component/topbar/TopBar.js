b51797cbe4689a19abfd38e4a93d58a003685bdb
Pradumna <pradumnabajoria@gmail.com>
2021-06-15 14:50:41 +0530

    created different folder for frontend and backend

--- client/src/component/topbar/TopBar.js
+++ client/src/component/topbar/TopBar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import "./TopBar.css"
 
 function TopBar() {
-    const user = true;
+    const user = false;
     return (
         <div className='top'>
             <div className='topLeft'>
